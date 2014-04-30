d59fe79bebcee92e81b1e7192e1dc9cd2b3492df
Artem Ivantsov <loky@Artems-MacBook-Pro.local>
2014-04-30 13:38:53 +0300

    switch to Berkshelf; add tests

---
+++ Berksfile
@@ -0,0 +1,8 @@
+site :opscode
+metadata
+cookbook 'zookeeper', git: "https://github.com/SimpleFinance/chef-zookeeper.git", ref: "49b9d05e913b3ad9f6e4751d00faf22b4fafa520"
+cookbook 'java', git: "https://github.com/opscode-cookbooks/java.git", ref: "7b559d6509c32cb250571250a2484dba2a9d5e49"
+cookbook 'build-essential', git: "https://github.com/opscode-cookbooks/build-essential.git", ref: "7f4d351036f10e7b641e1df137390491d6111669"
+cookbook 'cookbook-qubell-zookeeper', git: "https://github.com/loky9000/cookbook-qubell-zookeeper.git", ref: "10868f3a2518dd7e7e0f6263d15cd881fcd325e0"
+cookbook "minitest-handler"
+cookbook "zookeeper_component_test", path: "./test/cookbooks/zookeeper_component_test/"
---
+++ test/test.py
@@ -0,0 +1,57 @@
+import os
+import requests
+
+from test_runner import BaseComponentTestCase
+from qubell.api.private.testing import instance, environment, workflow, values
+
+@environment({
+    "default": {},
+    "AmazonEC2_CentOS_63": {
+        "policies": [{
+            "action": "provisionVms",
+            "parameter": "imageId",
+            "value": "us-east-1/ami-eb6b0182"
+        }, {
+            "action": "provisionVms",
+            "parameter": "vmIdentity",
+            "value": "root"
+        }]
+    },
+    "AmazonEC2_Ubuntu_1204": {
+        "policies": [{
+            "action": "provisionVms",
+            "parameter": "imageId",
+            "value": "us-east-1/ami-d0f89fb9"
+        }, {
+            "action": "provisionVms",
+            "parameter": "vmIdentity",
+            "value": "ubuntu"
+        }]
+    },
+    "AmazonEC2_Ubuntu_1004": {
+        "policies": [{
+            "action": "provisionVms",
+            "parameter": "imageId",
+            "value": "us-east-1/ami-0fac7566"
+        }, {
+            "action": "provisionVms",
+            "parameter": "vmIdentity",
+            "value": "ubuntu"
+        }]
+    }
+})
+class TomcatDevComponentTestCase(BaseComponentTestCase):
+    name = "component-tomcat-dev"
+    apps = [{
+        "name": name,
+        "file": os.path.realpath(os.path.join(os.path.dirname(__file__), '../%s.yml' % name))
+    }]
+
+     @instance(byApplication=name)
+     def test_zookeeper_ui(self, instance):
+        urls = instance.returnValues['output.zoo-ui']
+        for url in urls:
+        resp = requests.get(url, verify=False)
+
+        assert resp.status_code == 200    
+
